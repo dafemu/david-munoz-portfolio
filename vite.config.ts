ca9f6620d649067b44202a6e7e4f5173304bc010
David Munoz Barco <david.f.do@hotmail.com>
2026-07-28 21:44:30 -0500

    Set base path for portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/david-munoz-portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
